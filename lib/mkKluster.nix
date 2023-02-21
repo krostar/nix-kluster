a5122c48f4b9ebae3e3c0eb344fdde97377d8993
Alexis Destrez <alexis.destrez@pm.me>
2023-02-21 18:41:11 +0100

    Provide all extra attributes of mkKluster to lib.nixosSystem

--- lib/mkKluster.nix
+++ lib/mkKluster.nix
@@ -3,12 +3,13 @@
 returns a set nixos systems usable as flake's nixosConfigurations
 the provided $clustersTreeDir is a path to the clusters directory (as defined in readClustersTree.nix)
 */
-{defaultNixosModules ? []} @ args: clustersTreeDir: let
+{defaultNixosModules ? [], ...} @ args: clustersTreeDir: let
   clustersTree = import ./readClustersTree.nix {inherit lib;} clustersTreeDir;
   nodesList = import ./mkNodesList.nix {inherit lib;} clustersTree;
   mkNodeModulesList = import ./mkNodeModulesList.nix {inherit lib;} {
     inherit defaultNixosModules clustersTree clustersTreeDir;
   };
+  nixosSystemArg = builtins.removeAttrs args ["defaultNixosModules"];
 in
   builtins.listToAttrs (
     builtins.map (
@@ -24,7 +25,7 @@ in
           domain,
           node,
         } @ item:
-          lib.nixosSystem {modules = mkNodeModulesList item;}
+          lib.nixosSystem (nixosSystemArg // {modules = mkNodeModulesList item;})
       )
       nodesList
     )
