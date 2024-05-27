adb200a89105b50b8da00c3744fe95ed5d1bf994
Alexis Destrez <krostar@users.noreply.github.com>
2024-05-27 13:48:59 +0200

    Cleaning things up

--- flake.nix
+++ flake.nix
@@ -1,37 +1,72 @@
 {
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixos.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
   outputs = {
     self,
-    flake-utils,
+    nixos,
     nixpkgs,
     ...
-  }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [self.overlays.default];
-        };
-      in {
-        devShells.default = import ./shell.nix {inherit pkgs;};
-        checks = import ./nix/tests/lib {inherit pkgs;};
-      }
-    )
-    // {
-      lib = import ./nix/lib {inherit (nixpkgs) lib;};
-
-      overlays = {
-        default = _: prev: {
-          lib = prev.lib.extend (_: lib: {
-            kluster = import ./nix/lib {inherit lib;};
-          });
-        };
+  }: let
+    supportedSystems = ["aarch64-linux" "aarch64-darwin" "x86_64-linux" "x86_64-darwin"];
+    forEachSupportedSystems = f: builtins.listToAttrs (builtins.map (system: (nixpkgs.lib.nameValuePair system (f system))) supportedSystems);
+    pkgsForSystem = system: {
+      pkgs ? nixpkgs,
+      overlays ? [],
+    }: (import pkgs {inherit system overlays;});
+  in {
+    checks = forEachSupportedSystems (system: let
+      pkgs = pkgsForSystem system {
+        overlays = [self.overlays.default];
       };
+    in
+      import ./nix/tests/lib {inherit pkgs;});
 
-      nixosModules = import ./nixos/modules;
+    devShells = forEachSupportedSystems (system: let
+      pkgs = pkgsForSystem system {};
+    in {
+      default = pkgs.mkShellNoCC {
+        nativeBuildInputs = with pkgs; [
+          act
+          git
+          shellcheck
+          shfmt
+          yamllint
+
+          # go related
+          gci
+          go_1_22
+          gofumpt
+          golangci-lint
+          gotools
+          govulncheck
+
+          # nix related
+          alejandra
+          deadnix
+          manix
+          statix
+        ];
+        shellHook = ''
+          export CGO_ENABLED=0
+        '';
+      };
+    });
+
+    formatter = forEachSupportedSystems (system: let pkgs = pkgsForSystem system {}; in pkgs.alejandra);
+
+    lib = import ./nix/lib {inherit (nixos) lib;};
+
+    overlays = {
+      default = _: prev: {
+        lib = prev.lib.extend (_: lib: {
+          kluster = import ./nix/lib {inherit lib;};
+        });
+      };
     };
+
+    nixosModules = import ./nixos/modules;
+  };
 }
